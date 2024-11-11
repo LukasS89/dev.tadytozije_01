9455b8e9a8628946df27cbea3a66df5e8b9ff892
LukasS89 <lukas.sakala@gmail.com>
2024-11-11 21:56:27 +0100

    Update plasmic-host.tsx

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
 
-import Login from '../components/Login';
-
-
-registerComponent(Login, {
-  name: 'Login',  // Name of the component as it appears in Plasmic Studio
-  props: {
-    email: 'string',
-    password: 'string',
-  },
-  importPath: './components/Login',
-});
-
+// You can register any code components that you want to use here; see
+// https://docs.plasmic.app/learn/code-components-ref/
+// And configure your Plasmic project to use the host url pointing at
+// the /plasmic-host page of your nextjs app (for example,
+// http://localhost:3000/plasmic-host).  See
+// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
+// registerComponent(...)
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
